53128e8d32cc14a9a28ae612aa92c76d7c157e47
John Goerzen <jgoerzen@complete.org>
2018-10-03 22:00:16 -0500

    working on it

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(mtree-netbsd):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="mtree-netbsd" command="/usr/bin/mtree-netbsd"
--- debian/mtree-netbsd.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the mtree-netbsd package
-#
-0 4	* * *	root	[ -x /usr/bin/mtree-netbsd_maintenance ] && /usr/bin/mtree-netbsd_maintenance
